7a64200cbc7b8e0dca6586436236638112b069f7
davinci360 <jasonbrowntime@gmail.com>
2021-08-13 11:51:09 -0400

    readme line 35

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,4 +1,18 @@
-push test 
+function gameObject() {
+    return object1;
+}
 
+const object1;
+object1[home] = null;
+object1[away] = null;
 
-console.log(`Chicago in da house!`)
+    const home;
+    home[teamname] = "Bulls";
+    home[colors] = ["Red", "White"];
+    home[players] = null;
+
+        const players;
+        players[names] = null;
+        players[stats] = null;
+
+            const stats;
